434b7ea7d892c7ca736b4320bd85d9100869edaa
Yar.Karpov <yar.karpoov@gmail.com>
2024-08-14 16:49:36 +0300

    удалены 2 переменные

--- src/PetrolCalculatorDebug.java
+++ src/PetrolCalculatorDebug.java
@@ -4,8 +4,6 @@ public class PetrolCalculatorDebug {
     public static void main(String[] args) {
         new PetrolCalculatorDebug().calculate();
     }
- int X = 1;
-    int y=2;
     public void calculate() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите тип топлива: 1 - АИ-92, 2 - АИ-95, 3 - АИ-100 => ");
